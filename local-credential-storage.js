afeb7a5b981cc4cc137452aa73555cd65621ba11
Paul Melnikow <github@paulmelnikow.com>
2019-09-29 13:32:59 -0400

    Clean lint and reformat (#7)

--- local-credential-storage.js
+++ local-credential-storage.js
@@ -1,19 +1,23 @@
+'use strict'
+
 // A light wrapper around LocalStorage, for storing credentials.
 //
 // namespace: A dotted namespace to use within local storage.
 //   e.g. bodylabs.creel, bodylabs.creel.staging
 //
-var LocalCredentialStorage = module.exports = function (namespace) {
-    namespace = namespace || 'credentials';
+const LocalCredentialStorage = (module.exports = function(namespace) {
+  namespace = namespace || 'credentials'
 
-    this.usernameKey = namespace + '.access_key';
-    this.passwordKey = namespace + '.secret';
-};
+  this.usernameKey = `${namespace}.access_key`
+  this.passwordKey = `${namespace}.secret`
+})
 
-LocalCredentialStorage.prototype.isSet = function () {
-    return Boolean(localStorage.getItem(this.usernameKey)) &&
-        Boolean(localStorage.getItem(this.passwordKey));
-};
+LocalCredentialStorage.prototype.isSet = function() {
+  return (
+    Boolean(localStorage.getItem(this.usernameKey)) &&
+    Boolean(localStorage.getItem(this.passwordKey))
+  )
+}
 
 // Return an an object:
 //
@@ -23,27 +27,27 @@ LocalCredentialStorage.prototype.isSet = function () {
 //
 // Return null if credentials are not set.
 //
-LocalCredentialStorage.prototype.get = function () {
-    if (! this.isSet()) {
-        return null;
-    }
+LocalCredentialStorage.prototype.get = function() {
+  if (!this.isSet()) {
+    return null
+  }
 
-    return {
-        username: localStorage.getItem(this.usernameKey),
-        password: localStorage.getItem(this.passwordKey),
-    };
-};
+  return {
+    username: localStorage.getItem(this.usernameKey),
+    password: localStorage.getItem(this.passwordKey),
+  }
+}
 
-LocalCredentialStorage.prototype.set = function (username, password) {
-    if (! username || ! password) {
-        throw new Error('username and password should be truthy');
-    }
+LocalCredentialStorage.prototype.set = function(username, password) {
+  if (!username || !password) {
+    throw new Error('username and password should be truthy')
+  }
 
-    localStorage.setItem(this.usernameKey, username);
-    localStorage.setItem(this.passwordKey, password);
-};
+  localStorage.setItem(this.usernameKey, username)
+  localStorage.setItem(this.passwordKey, password)
+}
 
-LocalCredentialStorage.prototype.clear = function () {
-    localStorage.removeItem(this.usernameKey);
-    localStorage.removeItem(this.passwordKey);
-};
+LocalCredentialStorage.prototype.clear = function() {
+  localStorage.removeItem(this.usernameKey)
+  localStorage.removeItem(this.passwordKey)
+}
